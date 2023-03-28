fb8db1c8004ac45c14954c39420476d07862f83d
AilsaNBL27 <129130353+AilsaNBL27@users.noreply.github.com>
2023-03-28 11:49:48 +0700

    Add files via upload

---
+++ ailsa.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author C-13
+ */
+public class ailsa {
+    public static void main(String[] args) {
+        int nilaiA=10;
+         System.out.println("nilai A:"+ nilaiA);
+         Scanner inputnilai = new Scanner(System.in);
+         System.out.println("Inputkan nilai baru:");
+         nilaiA=inputnilai.nextInt();
+        
+        int nilaiB=10;
+         System.out.println("nilai B:"+ nilaiB);
+        double varone=13.2;
+         System.out.println("nilai:" + varone);
+        double vartwo=15.3;
+        System.out.println("nilai:" + vartwo);
+        
+        
+      boolean isPlus;
+      isPlus=true;
+      
+    }
+    
+}
